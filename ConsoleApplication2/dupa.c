603a2a313c5f5e46155b24a91e843f552befcbe4
peterstrzk <peterstrzk@gmail.com>
2024-04-24 22:03:30 +0200

    void function

--- ConsoleApplication2/dupa.c
+++ ConsoleApplication2/dupa.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
-int main() {
-	char dupa[] = "siema eniu";
 
-		int i;
-		for (i = 0; i < (sizeof(dupa) / sizeof(dupa[0])); i++) {
-			printf("%c\n",dupa[i]);
-		};
-	return 0;
+// Funkcja do wyszukiwania litery w ci¹gu znaków
+void znajdz_litere(char* str, char litera) {
+    char* ptr = str;
+    int znaleziono = 0;
+    int i;
+    for (i = 0; i < (sizeof(str)/sizeof(str)); i++) {
+        if (*ptr == litera) {
+            printf("Znaleziono literê '%c' na pozycji: %ld\n", litera, ptr - str);
+            znaleziono = 1;
+        }
+        ptr++;
+    }
+    if (!znaleziono) {
+        printf("Litera '%c' nie zosta³a znaleziona w ci¹gu.\n", litera);
+    }
+}
+
+int main() {
+    char ciag[] = "Dupa, sraka to epidemia taka";
+    char litera_do_znalezienia = 'a';
+
+    znajdz_litere(ciag, litera_do_znalezienia);
+
+    return 0;
 }
